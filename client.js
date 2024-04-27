9222c9895087d4d368dc90d965d87953ce4ebdcb
rnss <admin@rnss.club>
2024-04-27 15:42:39 +0100

    Added code!!

--- client.js
+++ client.js
@@ -1,3 +1,17 @@
-$(document).load(function () {
-    console.log("hi")
+$(window).on('load', function () {
+    let ReplaceCatImage = function () {
+        fetch("https://api.thecatapi.com/v1/images/search") // Performs a GET request to the API for cats!! :)
+            .then((response) => response.json()) // Turns response data into an actual JavaScript object so we can index it 
+            .then((json) => {
+                let catImage = json[0].url; // Finds the URL for the cat image in the object data
+
+                $("#catpicture").attr("src", catImage); // Sets the "source" element of the picture to the URL of the cat image we just got.
+            });
+    }
+
+    $("#buttonclick").click(function () { // When the button is clicked, it generates it
+        ReplaceCatImage()
+    })
+
+    ReplaceCatImage() // On page load set cat image
 });
